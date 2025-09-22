0d6ef67076fbb3e4c19a2ade313616907726b10d
dhevinaagustina <dhevinaagustina@gmail.com>
2025-09-22 17:41:26 +0700

    init: buat project tebak angka dengan tugas_mandiri.dart

--- JS3/tugas_mandiri/bin/tugas_mandiri.dart
+++ JS3/tugas_mandiri/bin/tugas_mandiri.dart
@@ -1,5 +1,24 @@
-import 'package:tugas_mandiri/tugas_mandiri.dart' as tugas_mandiri;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${tugas_mandiri.calculate()}!');
+void main() {
+  final random = Random();
+  final target = random.nextInt(100) + 1;
+
+  print("=== Game Tebak Angka ===");
+  print("Saya menyimpan angka antara 1-100. Coba tebak!");
+
+  int? tebakan;
+  do {
+    stdout.write("Masukkan tebakanmu: ");
+    tebakan = int.parse(stdin.readLineSync()!);
+
+    if (tebakan < target) {
+      print("Terlalu kecil!");
+    } else if (tebakan > target) {
+      print("Terlalu besar!");
+    } else {
+      print("Benar! Angkanya $target");
+    }
+  } while (tebakan != target);
 }
