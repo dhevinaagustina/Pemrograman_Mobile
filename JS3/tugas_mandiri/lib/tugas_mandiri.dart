62c482d2090b1f5230f040c42e3f26d1a68d4bd5
dhevinaagustina <dhevinaagustina@gmail.com>
2025-09-22 18:37:18 +0700

    merapikan struktur project

--- JS3/tugas_mandiri/lib/tugas_mandiri.dart
+++ JS3/tugas_mandiri/lib/tugas_mandiri.dart
@@ -1,3 +1,32 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:math';
+import 'input.dart';
+import 'hint.dart';
+
+class Game {
+  final int target = Random().nextInt(100) + 1;
+  int percobaan = 0;
+  bool selesai = false;
+
+  void mulai() {
+    print("=== Game Tebak Angka ===");
+    print("Saya menyimpan angka antara 1-100. Coba tebak!");
+
+    do {
+      int tebakan = InputHelper.bacaTebakan();
+      percobaan++;
+
+      if (tebakan == target) {
+        print("🎉 Benar! Angkanya $target (percobaan ke-$percobaan)");
+        selesai = true;
+      } else if (tebakan < target) {
+        print("Terlalu kecil!");
+      } else {
+        print("Terlalu besar!");
+      }
+
+      if (HintHelper.beriHint(percobaan, target)) {
+        selesai = true;
+      }
+    } while (!selesai);
+  }
 }
