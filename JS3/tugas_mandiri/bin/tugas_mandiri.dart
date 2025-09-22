77269db94db2067a6a389a02c8a3656b90115e8a
dhevinaagustina <dhevinaagustina@gmail.com>
2025-09-22 17:36:36 +0700

    Tugas Mandiri JS3

---
+++ JS3/tugas_mandiri/bin/tugas_mandiri.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_mandiri/tugas_mandiri.dart' as tugas_mandiri;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_mandiri.calculate()}!');
+}
---
+++ JS3/tugas_mandiri/test/tugas_mandiri_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_mandiri/tugas_mandiri.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
